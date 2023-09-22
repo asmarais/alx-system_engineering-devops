5c5cd62c584f6df8e302f0e25a57b9dc03ce95ea
asmarais <rais.asma99@gmail.com>
2023-09-22 11:44:37 +0100

    cm

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create a file named school in /tmp.
+file { '/tmp/school':
+  ensure  => 'file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
+
